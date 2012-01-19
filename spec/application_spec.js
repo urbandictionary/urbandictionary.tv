a9ba450a4827680df32f2d809feaed626bcc16c9
Aaron Peckham <apeckham@local>
2012-01-19 09:19:04 -0800

    tabs to spaces

--- spec/application_spec.js
+++ spec/application_spec.js
@@ -1,16 +1,16 @@
 describe("urbandictionary.tv", function() {
-	it("megaplaya_onvideoload", function() {
-		window.urls = [];
-		window.track_pageview = function() {};
-		window.megaplaya = {
-			api_getCurrentVideo: function() {
-				return {definition: "asdf", word: "the word"};
-			}
-		};
-		
-		megaplaya_onvideoload();
-		
-		expect($("#word_txt a").text()).toEqual("the word");
-		expect($("#word_txt a")).toHaveAttr("href", "http://www.urbandictionary.com/define.php?term=the+word");
-	});
+  it("megaplaya_onvideoload", function() {
+    window.urls = [];
+    window.track_pageview = function() {};
+    window.megaplaya = {
+      api_getCurrentVideo: function() {
+        return {definition: "asdf", word: "the word"};
+      }
+    };
+    
+    megaplaya_onvideoload();
+    
+    expect($("#word_txt a").text()).toEqual("the word");
+    expect($("#word_txt a")).toHaveAttr("href", "http://www.urbandictionary.com/define.php?term=the+word");
+  });
 });
