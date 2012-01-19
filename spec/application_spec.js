587f8ab666997ec8ba4bce74fa75aa2ba28a1318
Aaron Peckham <apeckham@local>
2012-01-19 09:25:14 -0800

    assertions to show that the def and example get printed to the page

--- spec/application_spec.js
+++ spec/application_spec.js
@@ -4,13 +4,19 @@ describe("urbandictionary.tv", function() {
     window.track_pageview = function() {};
     window.megaplaya = {
       api_getCurrentVideo: function() {
-        return {definition: "asdf", word: "the word"};
+        return {definition: "the bird [is the word]", word: "the word", example: "everybody knows the [bird] is the word"};
       }
     };
     
     megaplaya_onvideoload();
     
-    expect($("#word_txt a").text()).toEqual("the word");
+    expect($("#word_txt a")).toHaveText("the word");
     expect($("#word_txt a")).toHaveAttr("href", "http://www.urbandictionary.com/define.php?term=the+word");
+    
+    expect($("#definition_txt")).toHaveText("the bird is the word");
+    expect($("#definition_txt a")).toHaveText("is the word");
+    
+    expect($("#example_txt")).toHaveText("everybody knows the bird is the word");
+    expect($("#example_txt a")).toHaveText("bird");
   });
 });
