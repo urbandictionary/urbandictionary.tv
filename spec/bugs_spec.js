4b1f58dd37825d9e3254dceb52708849ac055013
Aaron Peckham <apeckham@local>
2012-02-13 21:04:03 -0500

    extract bugs notes

---
+++ spec/bugs_spec.js
@@ -0,0 +1,14 @@
+describe("bugs", function () {
+  describe("in the app", function () {
+    it("double encodes the word in Permalink.set on line 224");
+    it("doesn't pass the video URL to the VHX API, so the growl doesn't show");
+    it("doesn't HTML escape the video title in the growl");
+    it("splits the current word by spaces on line 446");
+  });
+
+  describe("in the tests", function () {
+    it("doesn't reset the HTML fixtures on every spec");
+    it("doesn't test when is_mobile is false");
+    it("doesn't reset all the globals in the app");
+  });
+});
